158b3f59f3ce02190d3c5f416288cb676ca54895
OEM Configuration (temporary user) <gustavo.prog40@gmail.com>
2026-04-25 15:49:44 -0300

    novo deploy

--- index.ts
+++ index.ts
@@ -1,21 +1,19 @@
-import express from 'express';
-import cors from 'cors'
-import dotenv from 'dotenv'
-import routes from './src/routes';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import routes from "./src/routes";
 
-  dotenv.config()
+dotenv.config();
 
 const app = express();
-const {PORT} = process.env;
+const { PORT } = process.env;
 
-
-app.use(cors())
-app.use(express.json())
-app.use(routes)
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
 
-
-export default app
+export default app;
